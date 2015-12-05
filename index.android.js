5e3e41b8ee1e2b95d60a5abfeaa35606342d2d93
sun zhuo <sunzhuo1228@126.com>
2015-12-06 00:04:28 +0800

    create main view and finish navigator

--- index.android.js
+++ index.android.js
@@ -1,98 +1,53 @@
 'use strict';
 
 var React = require('react-native');
+
 var {
     AppRegistry,
-    StyleSheet,
-    Text,
     View,
-    TextInput,
-    ToastAndroid,
+    Navigator,
+    Text,
+    BackAndroid,
+    StyleSheet,
     } = React;
 
-var KindleReact = React.createClass({
+var _navigator;
+var MainView = require('./main.android');
 
-    getInitialState: function() {
-        return {
-            inputText: '',
-        };
-    },
-
-    _clean() {
-        this.setState((state) => {
-            return {
-                inputText: '',
-            };
-        });
-    },
-
-    _preview() {
-        ToastAndroid.show('Press _preview Icon', ToastAndroid.SHORT)
-    },
-
-    _send() {
-        ToastAndroid.show('PressSend Icon', ToastAndroid.SHORT)
-    },
-
-
-
-    render: function () {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Kindle助手
-                </Text>
-                <View>
-                    <TextInput
-                        style={{height: 220, borderColor: 'gray', borderWidth: 1}}
-                        onChangeText={(inputText) => this.setState({inputText})}
-                        value={this.state.inputText}
-                        />
-                </View>
-                <View style={{
-                    flexDirection:'row'
-                    }
-                }>
-                    <Text style={styles.instructions}
-                          onPress={this._clean}
-                        >
-                        清除内容
-                    </Text>
-                    <Text style={styles.instructions}
-                          onPress={this._preview}
-                        >
-                        预览内容
-                    </Text>
-                </View>
-                <Text style={styles.instructions}
-                      onPress={this._send}
-                    >
-                    发送到我的kindle
-                </Text>
-            </View>
-        );
+BackAndroid.addEventListener('hardwareBackPress', () => {
+    if (_navigator && _navigator.getCurrentRoutes().length > 1) {
+        _navigator.pop();
+        return true;
     }
+    return false;
 });
 
-var styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+var RouteMapper = function (route, navigationOperations, onComponentRef) {
+    _navigator = navigationOperations;
+    if (route.name === 'main') {
+        return (
+            <MainView navigator={navigationOperations}/>
+        );
+    }
+
+};
+
+var KindleReact = React.createClass({
+    configureScene(route){
+        return Navigator.SceneConfigs.FadeAndroid;
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        height: 50,
-        borderWidth: 1,
-        alignItems: 'center',
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
+
+    render() {
+        return (
+            <Navigator
+                initialRoute={{name: 'main'}}
+                configureScene={this.configureScene}
+                renderScene={RouteMapper}/>
+        );
+    }
+
 });
 
 AppRegistry.registerComponent('KindleReact', () => KindleReact);
+
+module.exports = KindleReact;
---
+++ main.android.js
@@ -0,0 +1,98 @@
+'use strict';
+
+var React = require('react-native');
+var {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    ToastAndroid,
+    } = React;
+
+var MainView = React.createClass({
+
+    getInitialState: function() {
+        return {
+            inputText: '',
+        };
+    },
+
+    _clean() {
+        this.setState((state) => {
+            return {
+                inputText: '',
+            };
+        });
+    },
+
+    _preview() {
+        ToastAndroid.show('Press _preview Icon', ToastAndroid.SHORT)
+    },
+
+    _send() {
+        ToastAndroid.show('PressSend Icon', ToastAndroid.SHORT)
+    },
+
+
+
+    render: function () {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Kindle助手
+                </Text>
+                <View>
+                    <TextInput
+                        style={{height: 220, borderColor: 'gray', borderWidth: 1}}
+                        onChangeText={(inputText) => this.setState({inputText})}
+                        value={this.state.inputText}
+                        />
+                </View>
+                <View style={{
+                    flexDirection:'row'
+                    }
+                }>
+                    <Text style={styles.instructions}
+                          onPress={this._clean}
+                        >
+                        清除内容
+                    </Text>
+                    <Text style={styles.instructions}
+                          onPress={this._preview}
+                        >
+                        预览内容
+                    </Text>
+                </View>
+                <Text style={styles.instructions}
+                      onPress={this._send}
+                    >
+                    发送到我的kindle
+                </Text>
+            </View>
+        );
+    }
+});
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        height: 50,
+        borderWidth: 1,
+        alignItems: 'center',
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
+module.exports = MainView;
