bfe70322c629b6ea6239201a90fb06a05f7df7e6
sun zhuo <sunzhuo1228@126.com>
2015-12-12 22:19:53 +0800

    fix bug

--- android/app/src/main/java/com/kindleassistant/App.java
+++ android/app/src/main/java/com/kindleassistant/App.java
@@ -6,16 +6,6 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.text.TextUtils;
 import android.util.Log;
 
-import com.android.volley.Response.Listener;
-import com.kindleassistant.entity.UserCreateApi.UserCreateRqt;
-import com.kindleassistant.entity.UserCreateApi.UserCreateRsp;
-import com.kindleassistant.manager.UpdateMgr;
-import com.kindleassistant.manager.UserInfoMgr;
-import com.kindleassistant.manager.VolleyMgr;
-import com.kindleassistant.net.GsonRequest;
-import com.kindleassistant.utils.ChannelUtil;
-import com.tencent.android.tpush.XGIOperateCallback;
-import com.tencent.android.tpush.XGPushManager;
 import com.umeng.analytics.AnalyticsConfig;
 import com.umeng.analytics.MobclickAgent;
 import com.umeng.fb.FeedbackAgent;
@@ -40,8 +30,6 @@ public class App extends Application {
         // 友盟的配置
         // 调试模式
         MobclickAgent.setDebugMode(BuildConfig.DEBUG);
-        // 使用在线配置功能
-        MobclickAgent.updateOnlineConfig(getContext());
         // 检查友盟的res是否都复制进去了
         UmengUpdateAgent.setUpdateCheckConfig(BuildConfig.DEBUG);
         UmengUpdateAgent.setUpdateOnlyWifi(false);
